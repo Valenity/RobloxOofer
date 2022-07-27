32a3e9a117c3b759ba8985fa08e96dcbf176a8e3
Anti <antidecals@gmail.com>
2022-07-27 10:14:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+using System.Media;
+
+namespace RobloxOofer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Restoring Roblox Death Sound");
+            Console.WriteLine("\nReplacing ouch.ogg at " + Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Roblox\Versions\version-9045f70ea522489c\content\sounds\ouch.ogg");
+            Thread.Sleep(2000);
+            var n = new WebClient();
+            try
+            {
+                n.DownloadFile("https://cdn.discordapp.com/attachments/893283087570595840/1001869210860929024/ouch.ogg", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Roblox\Versions\version-9045f70ea522489c\content\sounds\ouch.ogg");
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Replacement Failed!\n\nAre you connected to the internet? :(");
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Replacement Success!\n\nNow go enjoy that oof!");
+            Console.ReadLine();
+        }
+    }
+}
